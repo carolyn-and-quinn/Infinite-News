7068fb9e3d6c2c6e4980c216ac5531d44383245c
Carolyn McNeillie <hello@carolynmcneillie.com>
2018-02-18 11:29:04 -0500

    Pulling in Quinn's changes to js

--- js/styles.js
+++ js/styles.js
@@ -1,1 +1,19 @@
 console.log("Styles link is working");
+
+newsApp.selectArticlesToStyle = () => {
+    $('article:has(h2):has(.publication):has(.author):has(.preview-text):has(img):has(a)').addClass('full-data-set');
+
+
+
+    $('article:not(:has(img))').addClass('no-image');
+
+
+
+
+
+    // if ($('article:has(img)') && $('article:has(.author)')) {
+    //     this.addClass('this-class');
+    // }
+
+    // }
+}
