d6dee4786cd7f8f1de0070b9a6be1df0c53cc439
Carolyn McNeillie <hello@carolynmcneillie.com>
2018-02-18 12:09:50 -0500

    Cleaning up the grid

--- js/styles.js
+++ js/styles.js
@@ -3,17 +3,6 @@ console.log("Styles link is working");
 newsApp.selectArticlesToStyle = () => {
     $('article:has(h2):has(.publication):has(.author):has(.preview-text):has(img):has(a)').addClass('full-data-set');
 
-
-
     $('article:not(:has(img))').addClass('no-image');
 
-
-
-
-
-    // if ($('article:has(img)') && $('article:has(.author)')) {
-    //     this.addClass('this-class');
-    // }
-
-    // }
 }
