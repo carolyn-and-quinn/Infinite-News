2808ff11d422f8c852156b347fb5fb463c9f687a
Carolyn McNeillie <hello@carolynmcneillie.com>
2018-02-18 13:37:55 -0500

    Styling in a good place

--- js/styles.js
+++ js/styles.js
@@ -1,7 +1,7 @@
 console.log("Styles link is working");
 
 newsApp.selectArticlesToStyle = () => {
-    $('article:has(h2):has(.publication):has(.author):has(.preview-text):has(img):has(a)').addClass('full-data-set');
+    $('article:has(h2):has(.preview-text):has(img):has(a)').addClass('full-data-set');
 
     $('article:not(:has(img))').addClass('no-image');
 
